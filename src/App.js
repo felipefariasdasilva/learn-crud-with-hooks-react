b1eb6ccc77f670c0eb1121c657b812f7917ed10b
felipefariasdasilva <feliipefarias@outlook.com>
2020-03-06 13:19:38 -0300

    added delete and update feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
-const App = () =>{
+const App = () => {
 
   const usersData = [
     { id: 1, name: 'Tania', username: 'floppydiskette' },
@@ -10,21 +12,66 @@ const App = () =>{
   ]
 
   const [users, setUsers] = useState(usersData)
+  const [editing, setEditing] = useState(false)
 
-  return(
-    <div className = "container">
+  const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  const addUser = user => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  }
+
+  const deleteUser = id => {
+    setEditing(false)
+    setUsers(users.filter(user => user.id !== id))
+  }
+
+  const updateUser = (id, updateUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updateUser : user)))
+  }
+
+
+  return (
+    <div className="container">
       <h1>CRUD with Hooks</h1>
-      <div className = "flex-row">
-        <div className = "flex-large">
-          <h2>Add User</h2>
+      <div className="flex-row">
+
+        <div className="flex-large">
+          {
+            editing ? (
+              <div>
+                <h2>Edit User</h2>
+                <EditUserForm
+                  editing={editing}
+                  setEditing={setEditing}
+                  currentUser={currentUser}
+                  updateUser={updateUser}
+                />
+              </div>
+            ) : (
+                <div>
+                  <h2>Add User</h2>
+                  <AddUserForm addUser={addUser} />
+                </div>
+              )
+          }
         </div>
-        <div className = "flex-large">
+
+        <div className="flex-large">
           <h2>View User</h2>
-          <UserTable users={users}/>
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
+
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -17,9 +17,11 @@ const UserTable = props => (
                         <td>{ user.name }</td>
                         <td>{ user.username }</td>
                         <td>
-                    <button className = "button muted-button">Edit</button>
-                    <button className = "button muted-button">Delete</button>
-                </td>
+                            <button onClick={() => {props.editRow(user)}}
+                                    className = "button muted-button">Edit</button>
+                            <button onClick={() => props.deleteUser(user.id)} 
+                                    className = "button muted-button">Delete</button>
+                        </td>
                     </tr>
                 ))
             ) : (
