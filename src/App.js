97e2ed8c39f94ea8558db85428e639e220644309
felipefariasdasilva <feliipefarias@outlook.com>
2020-03-06 08:16:29 -0300

    first commit: create user table with hooks

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable'
+
+const App = () =>{
+
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
+  return(
+    <div className = "container">
+      <h1>CRUD with Hooks</h1>
+      <div className = "flex-row">
+        <div className = "flex-large">
+          <h2>Add User</h2>
+        </div>
+        <div className = "flex-large">
+          <h2>View User</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
